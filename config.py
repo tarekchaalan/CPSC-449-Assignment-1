21594fd3bdf878062f831aa66e9390e57d545d98
Tarek Chaalan <tchaalan23@icloud.com>
2024-03-19 19:07:09 -0700

    Updated URI to include database name

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 class Config(object):
-    # Replace USERNAME and PASSWORD with your MySQL credentials
-    #                                             v        v
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://USERNAME:PASSWORD@localhost/TarekChaalan-449a1'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
